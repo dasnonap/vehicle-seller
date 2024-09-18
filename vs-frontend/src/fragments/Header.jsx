9fd4f748fac54b720c2ec84172334dfe3384977f
dasnonap <ivanmihov971@gmail.com>
2024-09-18 07:31:19 +0300

    feat(frontend/setup): add react router and basic elements

---
+++ vs-frontend/src/fragments/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <div>
+            <h3>Header content</h3>
+        </div>
+    )   
+}
+
+export default Header;
--- vs-frontend/src/main.jsx
+++ vs-frontend/src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'register',
+        element: <Register />
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
